54d2890e7540a7cb42eed546d1442225bb0eface
Luiz Alves <luizlethal338@gmail.com>
2026-03-16 19:15:13 -0300

    add files dentro do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,24 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config
     },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 30000,
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    video: true,
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: true,
+    html: true,
+    json: true,
+    timestamp: 'mmddyyyy_HHMMss'
+  }
+})
