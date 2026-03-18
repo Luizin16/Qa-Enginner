0cc6c7c18725c6f1b00d7f7540159d69343a9f89
Luiz Alves <luizlethal338@gmail.com>
2026-03-17 22:40:00 -0300

    add news files

--- cypress.config.js
+++ cypress.config.js
@@ -2,23 +2,15 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      return config
-    },
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 30000,
     viewportWidth: 1366,
     viewportHeight: 768,
-    video: true,
+    chromeWebSecurity: false,
     screenshotOnRunFailure: true,
-    chromeWebSecurity: false
-  },
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'mochawesome-report',
-    overwrite: true,
-    html: true,
-    json: true,
-    timestamp: 'mmddyyyy_HHMMss'
+    video: true,
+    setupNodeEvents(on, config) {
+      return config
+    }
   }
 })
